4d149f8af633ecd30815f5dd7fbdead130a133f4
GiovanniDevs <damico.giovanni.90@gmail.com>
2025-12-16 09:03:54 +0000

    add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
   for (let button of buttons) {
     button.addEventListener("click", function () {
       if (this.getAttribute("data-type") === "submit") {
-        alert("You clicked submit");
+        checkAnswer();
       } else {
         let gameType = this.getAttribute("data-type");
         runGame(gameType);
@@ -32,9 +32,34 @@ function runGame(gameType) {
   }
 }
 
-function checkAnswer() {}
+function checkAnswer() {
+  let userAnswer = parseInt(document.getElementById("answer-box").value);
+  let calculatedAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
 
-function calculateCorrectAnswer() {}
+  if (isCorrect) {
+    alert("hey u got it right");
+  } else {
+    alert(`the correct answer was ${calculatedAnswer[0]}`);
+  }
+
+  runGame(calculatedAnswer[1]);
+}
+
+/** Gets all the values directly from the DOM in order to avoid declaring global variables
+ */
+function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.getElementById("operand1").innerText);
+  let operand2 = parseInt(document.getElementById("operand2").innerText);
+  let operator = document.getElementById("operator").innerText;
+
+  if (operator === "+") {
+    return [operand1 + operand2, "addition"];
+  } else {
+    alert(`unknown operator: ${operator}`);
+    throw `unknown operator, aborting`;
+  }
+}
 
 function incrementScore() {}
 
